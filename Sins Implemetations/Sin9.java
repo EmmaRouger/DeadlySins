9b86d7e42f58b9b2cd230478b5e568fb10ef1d90
Mason Myre <82189404+Lancer077@users.noreply.github.com>
2024-04-26 17:50:17 -0500

    comment Sin9.java

--- Sins Implemetations/Sin9.java
+++ Sins Implemetations/Sin9.java
@@ -7,19 +7,19 @@ import java.util.Scanner;
 
 public class Sin9 {
 
-    public void readFileBadExample(String fileName){
+    public void readFileBadExample(String fileName){ //this is the bad example of how to catch exceptions when reading in a file
         String firstLine = "";
-        try{
-            FileReader file = new FileReader(fileName);
+        try{ //here is our try block, complete with lines that have absolutely nothing to do with the try statement
+            FileReader file = new FileReader(fileName); //this is the part that we are worried about failing
             BufferedReader in = new BufferedReader(file);
-            String nextLine = in.readLine();
-            firstLine = nextLine;
-            while(!in.equals(null)){
-                System.out.println(nextLine);
+            String nextLine = in.readLine(); //
+            firstLine = nextLine; //imagine we also want the first line of the file that we are reading in to be printed out at the end
+            while(!in.equals(null)){ //while we have another line
+                System.out.println(nextLine); //we print out the line
                 nextLine = in.readLine();
             }
         }
-        catch(IOException e){
+        catch(IOException e){ //random catch, displaying no information
         }
         //this is a logic error
         //although it is obvious that I am trying to get the first line
@@ -28,31 +28,31 @@ public class Sin9 {
 
     }
 
-    public void readFileGoodExample(String fileName){
-        String firstLine = "";
-        FileReader file;
+    public void readFileGoodExample(String fileName){ //better example of how to handle exceptions for reading in a file
+        String firstLine = ""; 
+        FileReader file; //creating our variables beforehand
         BufferedReader in;
         String nextLine;
         
         try{
-            file = new FileReader(fileName);
+            file = new FileReader(fileName); //no longer declaring the variables in the try statement, instead we now merely initialize them
             in = new BufferedReader(file);
-            nextLine = in.readLine();
-            firstLine = nextLine;
-            while (!in.equals(null)) {
-                System.out.println(nextLine);
+            nextLine = in.readLine(); //get the next line
+            firstLine = nextLine; //get the first line to be printed out at the end
+            while(!in.equals(null)) { //while we have another line
+                System.out.println(nextLine); //print that line
                 nextLine = in.readLine();
             }
-            in.close();
+            in.close(); //close the file correctly
         }
-        catch(FileNotFoundException e){
-            System.out.println("Error, file was not found: " + fileName);
+        catch(FileNotFoundException e){ //specific exception error handling
+            System.out.println("Error, file was not found: " + fileName); //printing out errors with meaninful information
         }
         catch(IOException e){
             System.out.println("Error reading from the file " + fileName);
         }
         finally{
-            System.out.println(firstLine);
+            System.out.println(firstLine); //only print out the first line at the end if it actually got assigned
         }
 
     }
